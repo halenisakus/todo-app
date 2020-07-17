1b2c0f09d69395c6e342a1ce5ed2d4d0f3d38ef7
halenisakus <halenisakus@gmail.com>
2020-07-17 17:03:59 +0300

    Updates

---
+++ precache-manifest.8c88910bd8b3b6eff65e9ac6c0577938.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856dd659b7549616234d93424061b42d",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "b9e6950e560dd1edd3aa",
+    "url": "/todoapp/static/css/main.8cac1284.chunk.css"
+  },
+  {
+    "revision": "79de567c1254fc5ca901",
+    "url": "/todoapp/static/js/2.098b3e37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.098b3e37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9e6950e560dd1edd3aa",
+    "url": "/todoapp/static/js/main.e3332aa5.chunk.js"
+  },
+  {
+    "revision": "566fce36fccc7e3661a2",
+    "url": "/todoapp/static/js/runtime-main.9b29aac0.js"
+  }
+]);
